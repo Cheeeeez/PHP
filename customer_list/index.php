60bb9c5214b7d14800de8f37e2fcae3837276cd8
Nam <nguyentrongnama8@gmail.com>
2020-04-06 14:41:26 +0700

    add customer_filter

--- customer_list/index.php
+++ customer_list/index.php
@@ -5,29 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
-
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th, td {
-            padding: 8px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-
-        .thumbnail {
-            height: 60px;
-            width: 100px;
-            overflow: hidden;
-        }
-
-        .thumbnail img {
-            width: 100%;
-    </style>
 </head>
 <body>
 <table border="0">
